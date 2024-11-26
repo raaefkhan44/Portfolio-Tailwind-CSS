8a311813d3aa444e0ec76567d3f4ad5b81fdfa46
Raaef Khan <mahmoodulhaq539@gmail.com>
2024-11-27 01:00:14 +0500

    changes done

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -70,7 +70,7 @@ export default function Project() {
             </a>
           </button>
         </div>
-
+{/* Simple Calculator */}
         <div className="bg-black text-white rounded-2xl shadow-md p-5 transition duration-300 hover:-translate-y-2 flex flex-col items-center h-full">
           <Image
             src="/calculator.PNG"
@@ -80,16 +80,39 @@ export default function Project() {
             className="rounded-2xl w-full h-48 object-cover transition-transform duration-300 hover:scale-105 hover:brightness-110"
           />
           <h3 className="text-xl font-bold text-[#f0a500] mb-2 mt-4 transition-colors duration-300 hover:text-blue-500">
-            Simpla Calculator
+            Simple Calculator
           </h3>
           <p className="text-lg leading-relaxed text-gray-300 mb-5 text-center">
-            A website powered by Next.js and TypeScript, featuring a simple calculator for performing basic arithmetic operations.          </p>
+          The Simple Calculator is an intuitive web application designed for basic arithmetic operations. Users can perform addition, subtraction, multiplication, and division with ease. The interface features two input fields for entering numbers, a display for the result, and clearly labeled buttons for each operation.          </p>
           <button className="bg-[#f0a500] font-bold border-none rounded-md text-white p-2 transition-colors duration-300 hover:bg-blue-500 hover:scale-105">
             <a href="https://portfolio-tailwind-css-rosy.vercel.app/" target="_blank" className="text-white">
               View Calculator
             </a>
           </button>
         </div>
+
+        {/* Digital Clock */}
+        <div className="bg-black text-white rounded-2xl shadow-md p-5 transition duration-300 hover:-translate-y-2 flex flex-col items-center h-full">
+          <Image
+            src="/digital clock.PNG"
+            alt="digital Clock"
+            width={300}
+            height={250}
+            className="rounded-2xl w-full h-48 object-cover transition-transform duration-300 hover:scale-105 hover:brightness-110"
+          />
+          <h3 className="text-xl font-bold text-[#f0a500] mb-2 mt-4 transition-colors duration-300 hover:text-blue-500">
+            Digital Calculator
+          </h3>
+          <p className="text-lg leading-relaxed text-gray-300 mb-5 text-center">
+          The Digital Clock is a user-friendly component that displays the current time in a clear and visually appealing format. It offers two modes: a 24-hour format for those who prefer military time, and a 12-hour format for a traditional view. The clock updates every second, ensuring real-time accuracy.
+          </p>
+          <button className="bg-[#f0a500] font-bold border-none rounded-md text-white p-2 transition-colors duration-300 hover:bg-blue-500 hover:scale-105">
+            <a href="https://portfolio-tailwind-css-rosy.vercel.app/" target="_blank" className="text-white">
+              View Clock
+            </a>
+          </button>
+        </div>
+
       </div>
     </div>
   );
